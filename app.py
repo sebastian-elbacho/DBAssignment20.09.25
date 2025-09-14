b12b499114a2f59975d0a691fc4ef94434d2fd9c
SebastianK <sebastian.bachowski@gmail.com>
2025-09-14 20:28:41 +0100

    fixed

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your-secret-key-here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/portfolio_db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/portfolio_db'
 
 # Initialize SQLAlchemy
 db = SQLAlchemy(app)
