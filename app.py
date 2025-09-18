12f0c1447512afe57f70c5e640985ae1c98ee0bf
SebastianK <sebastian.bachowski@gmail.com>
2025-09-18 10:27:49 +0100

    not workimng again

--- app.py
+++ app.py
@@ -119,6 +119,7 @@ def edit_project(id):
         project.description = request.form['description']
         project.technologies = request.form['technologies']
         db.session.commit()
+        flash(f"Projekt '{project.title}' updated", "info" )
 
         
         return redirect(url_for('projects'))
