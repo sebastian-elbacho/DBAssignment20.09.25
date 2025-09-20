d439e02c4e3b94482d9e9e7cd1e7d679888a0e3b
SebastianK <sebastian.bachowski@gmail.com>
2025-09-20 19:48:14 +0100

    init_db()do stworzenia tabel

--- app.py
+++ app.py
@@ -213,13 +213,27 @@ def create_app():
     app.config["SQLALCHEMY_DATABASE_URI"] = database_url
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+   def create_app():
+    app = Flask(__name__)
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'domyslny-klucz')
+    database_url = os.getenv("DATABASE_URL")
+    if not database_url:
+        raise RuntimeError("Brak DATABASE_URL")
+
+    if database_url.startswith("postgres://"):
+        database_url = database_url.replace("postgres://", "postgresql://", 1)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_url
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
     db.init_app(app)
 
     with app.app_context():
-        db.create_all()
-        print(">>> Tabele zostały utworzone lub już istniały")
+        init_db()  # <- to jest teraz klucz
 
     return app
+
 
 
 
